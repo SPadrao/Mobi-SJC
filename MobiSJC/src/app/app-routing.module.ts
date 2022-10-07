9ee2716a9b08c06477e68c42cc5e772d17fa3998
taistos <114361031+taistos@users.noreply.github.com>
2022-10-07 13:10:34 -0300

    Botões para passageiros e motoristas

--- MobiSJC/src/app/app-routing.module.ts
+++ MobiSJC/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
+import { LoginCaronasP1Component } from './login-caronas-p1/login-caronas-p1.component';
 import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes=[
   {path: '', component: HomeComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'cadastrar', component:CadastroComponent}
+  {path: 'cadastrar', component:CadastroComponent},
+  {path: 'caronasP1', component:LoginCaronasP1Component}
 ];
 
 @NgModule({
