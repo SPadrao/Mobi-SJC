81111685d18c4d63ec2f9ebf84c9c0d12af33420
thamirexsl <98592236+thamirexsl@users.noreply.github.com>
2022-10-22 14:24:48 -0300

    Cadastro e Login - conectados com o banco

--- MobiSJC/src/app/app-routing.module.ts
+++ MobiSJC/src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
-import { CaronaComponent } from './carona/carona.component';
-import { HomeComponent } from './home/home.component';
-import { LoginCaronasP1Component } from './login-caronas-p1/login-caronas-p1.component';
-import { CreateRideFormComponent } from './create-ride-form/create-ride-form.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { EsqueceuSenhaComponent } from './component/esqueceu-senha/esqueceu-senha.component';
+import { RegisterComponent } from './component/register/register.component';
+import { VarifyEmailComponent } from './component/varify-email/varify-email.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes=[
-  {path: '', component: HomeComponent},
-  {path: 'cadastrar', component:CadastroComponent},
-  {path: 'registrarCarona', component: CreateRideFormComponent},
+
+const routes: Routes = [
   {path: '',redirectTo: 'login',pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'login-caronas-p1', component: LoginCaronasP1Component}
-
-
+  {path: 'login', component : LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'register', component : RegisterComponent},
+  {path : 'varify-email', component : VarifyEmailComponent},
+  {path: 'esqueceu-senha', component : EsqueceuSenhaComponent}
 ];
 
 @NgModule({
--- MobiSJC/src/app/app.component.spec.ts
+++ MobiSJC/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MobiSJC'`, () => {
+  it(`should have as title 'firebase-angular-mobisjc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MobiSJC');
+    expect(app.title).toEqual('firebase-angular-mobisjc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MobiSJC app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('firebase-angular-mobisjc app is running!');
   });
 });
