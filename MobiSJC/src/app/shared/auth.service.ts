177265b541a5cb6831cf5c95e7a127d58ca069af
Renan Bueno Angioletto <renan.angioletto@gmail.com>
2022-10-25 16:25:33 -0300

    added func to save data upon sign up

--- MobiSJC/src/app/shared/auth.service.ts
+++ MobiSJC/src/app/shared/auth.service.ts
@@ -1,34 +1,76 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { User } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor(private fireauth : AngularFireAuth, private router: Router) { }
+export class AuthService {
+  userData: any; // Save logged in user data
+  constructor(private fireauth: AngularFireAuth, private firestore: AngularFirestore, private router: Router) {
+    /* Saving user data in localstorage when 
+    logged in and setting up null when logged out */
+    this.fireauth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
+
+  // Returns true when user is looged in and email is verified
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
+  }
+
+  /* Setting up user data when sign in with username/password, 
+  sign up with username/password and sign in with social auth  
+  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.firestore.doc(
+      `users/${user.uid}`
+    );
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
 
   // login method
-  login(email : string, senha : string) {
-    this.fireauth.signInWithEmailAndPassword(email,senha).then( res => {
-        localStorage.setItem('token','true');
+  login(email: string, senha: string) {
+    this.fireauth.signInWithEmailAndPassword(email, senha).then(res => {
+      localStorage.setItem('token', 'true');
+      this.SetUserData(res.user);
 
-        if(res.user?.emailVerified == true) {
-          this.router.navigate(['login-caronas-p1']);
-        } else {
-          this.router.navigate(['/verificar-email']);
-        }
+      if (res.user?.emailVerified == true) {
+        this.router.navigate(['login-caronas-p1']);
+      } else {
+        this.router.navigate(['/verificar-email']);
+      }
 
     }, err => {
-        alert(err.message);
-        this.router.navigate(['/login']);
+      alert(err.message);
+      this.router.navigate(['/login']);
     })
   }
 
   //register method 
-  cadastro(email : string, senha : string) {
-    this.fireauth.createUserWithEmailAndPassword(email, senha).then( res => {
+  cadastro(email: string, senha: string) {
+    this.fireauth.createUserWithEmailAndPassword(email, senha).then(res => {
       alert('Cadastro feito com sucesso');
       this.sendEmailForVarification(res.user);
       this.router.navigate(['/login']);
@@ -39,7 +81,7 @@ export class AuthService {
   }
   // sign out
   logout() {
-    this.fireauth.signOut().then( () => {
+    this.fireauth.signOut().then(() => {
       localStorage.removeItem('token');
       this.router.navigate(['/login']);
     }, err => {
@@ -48,13 +90,12 @@ export class AuthService {
   }
 
   //email para verificação 
-  sendEmailForVarification(user : any) {
-
+  sendEmailForVarification(user: any) {
     this.fireauth.currentUser.then(u => u?.sendEmailVerification())
-      .then(() =>{
+      .then(() => {
         this.router.navigate(['/verificar-email']);
-      }, (err: any) =>{
-          alert('Something Went Wrong. Not able to send mail to registered Email.');
+      }, (err: any) => {
+        alert('Something Went Wrong. Not able to send mail to registered Email.');
       })
 
   }
