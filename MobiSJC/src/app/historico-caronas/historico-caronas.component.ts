48005b7f9d1770124e00f52d7da6a3c7c4cf2656
ViniciusTSato <vsato@unifesp.br>
2022-12-07 11:38:04 -0300

    Roteamento menu <-> historico

--- MobiSJC/src/app/historico-caronas/historico-caronas.component.ts
+++ MobiSJC/src/app/historico-caronas/historico-caronas.component.ts
@@ -10,7 +10,7 @@ import { RideService } from '../shared/ride.service';
 })
 export class HistoricoCaronasComponent implements OnInit {
 
-  rideList: Ride[] = [];
+  rideList : Ride[] = [];
   studentObj: Ride = {
     uid: '',
     id: '',
@@ -20,18 +20,18 @@ export class HistoricoCaronasComponent implements OnInit {
     embark: '',
     vacancy: 0
   };
-  uid: string = '';
-  id: string = '';
-  origin: string = '';
+  uid : string = '';
+  id : string = '';
+  origin : string = '';
   destiny: string = '';
   price: number = 0;
-  embark: string = '';
+  embark : string = '';
   vacancy: number = 0;
   AuthService: any;
   RideService: any;
 
 
-  constructor(private auth: AuthService, private data: RideService) { }
+  constructor(private auth: AuthService, private data : RideService) { }
 
   ngOnInit(): void {
     this.getPastrides();
@@ -41,7 +41,7 @@ export class HistoricoCaronasComponent implements OnInit {
   Rides: any;
 
 
-  getPastrides() {
+  getPastrides(){
 
     this.data.getPastrides(this.auth.userData.uid).subscribe(res => {
 
